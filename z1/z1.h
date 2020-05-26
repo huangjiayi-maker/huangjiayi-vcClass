ab47d16ae0ffa13c21adfbc939eb339008c6ee93
huangjiayi-maker <61820262+huangjiayi-maker@users.noreply.github.com>
2020-05-26 20:18:49 +0800

    2020.5.26提交第十二周综合实验一

---
+++ z1/z1.h
@@ -0,0 +1,33 @@
+
+// z1.h : z1 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// Cz1App:
+// 有关此类的实现，请参阅 z1.cpp
+//
+
+class Cz1App : public CWinApp
+{
+public:
+	Cz1App();
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Cz1App theApp;
---
+++ z1/z1View.h
@@ -0,0 +1,66 @@
+
+// z1View.h : Cz1View 类的接口
+//
+
+#pragma once
+
+class Cz1Set;
+
+class Cz1View : public CRecordView
+{
+protected: // 仅从序列化创建
+	Cz1View();
+	DECLARE_DYNCREATE(Cz1View)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_Z1_FORM };
+#endif
+	Cz1Set* m_pSet;
+	void draw_pic(CString file);
+
+// 特性
+public:
+	Cz1Doc* GetDocument() const;
+
+// 操作
+public:
+	CString s;
+
+// 重写
+public:
+	virtual CRecordset* OnGetRecordset();
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+	virtual void OnInitialUpdate(); // 构造后第一次调用
+
+// 实现
+public:
+	virtual ~Cz1View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangeEdit1();
+	CString path;
+	afx_msg void OnRecordFirst();
+	afx_msg void OnRecordPrev();
+	afx_msg void OnRecordNext();
+	afx_msg void OnRecordLast();
+	virtual void OnPrint(CDC* pDC, CPrintInfo* pInfo);
+	afx_msg void OnPaint();
+};
+
+#ifndef _DEBUG  // z1View.cpp 中的调试版本
+inline Cz1Doc* Cz1View::GetDocument() const
+   { return reinterpret_cast<Cz1Doc*>(m_pDocument); }
+#endif
+
